af84815af88fbb134c09cf80b26baa306c82a756
BioVstu <biovstu@gmail.com>
2023-05-15 16:34:42 +0300

    adding info

--- Extract/From_dadata.py
+++ Extract/From_dadata.py
@@ -4,25 +4,29 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 
-basic = HTTPBasicAuth('odata_erp'.encode('utf-8'), 'oeLrXMT1Eryp9YM9P8st'.encode('utf-8'))
+LOGIN = ''
+PASSWORD = ''
+ODATASERVER = ''
+# Здесь может быть код работы с данными
+basic = HTTPBasicAuth(LOGIN.encode('utf-8'), PASSWORD.encode('utf-8'))
 refs = 'Ref_Key,Description'
-buff_resp = requests.get(f"http://dcr-iis03/erp_data/odata/standard.odata/Catalog_СегментыНоменклатуры?$select={refs}", headers=dict(Accept='application/json;odata=nometadata'), auth=basic)
+buff_resp = requests.get(f"{ODATASERVER}/Catalog_СегментыНоменклатуры?$select={refs}", headers=dict(Accept='application/json;odata=nometadata'), auth=basic)
 buff_df = pd.read_json(json.dumps(buff_resp.json()['value']))
 
 from dadata import Dadata
 
-token = "1a92df4e58672fe394d72da001c6022a90936540"
-secret = "ebca67c1e0b63922f82f07b51ce149cea4850e25"
+token = ''
+secret = ''
 
 with Dadata(token, secret) as dadata:
-    for i,adres in enumerate(download_data['Представление']):
+    for i,adres in enumerate(buff_df['Представление']):
 #         print(f'{i} - {adres}')
         js = dadata.clean(name='address', source=adres)
         try:
             df = pd.DataFrame(js, index=[0])
         except ValueError:
             df = pd.DataFrame(js).head(1)
-        df['Ref_Key'] = download_data.loc[i,'Ref_Key']
+        df['Ref_Key'] = buff_df.loc[i,'Ref_Key']
         if i == 0:
             output_frame = df
         else:
