a7ece0a1530e099f417c277a7007ff4389a03836
BioVstu <biovstu@gmail.com>
2023-06-08 17:45:23 +0300

    CenaNomenklatury applyed to supliers

--- Others/CenaNomenklatury.py
+++ Others/CenaNomenklatury.py
@@ -5,12 +5,14 @@ import requests
 from requests.auth import HTTPBasicAuth
 import time
 
-print('Начнем!')
+t = time.strftime('%X', time.localtime(time.time()))
+print(f'{t} - Начнем!')
 # формируем аутентификацию
 basic = HTTPBasicAuth('odata_erp'.encode('utf-8'), 'oeLrXMT1Eryp9YM9P8st'.encode('utf-8'))
 # делаем запрос OData
-buff_resp = requests.get(f"http://dcr-iis03/erp_data/odata/standard.odata/InformationRegister_ЦеныНоменклатуры", headers=dict(Accept='application/json;odata=nometadata'), auth=basic)
-print(f'Ответ получен - {buff_resp}')
+buff_resp = requests.get(f"http://dcr-iis03/erp_data/odata/standard.odata/InformationRegister_ЦеныНоменклатурыПоставщиков", headers=dict(Accept='application/json;odata=nometadata'), auth=basic)
+t = time.strftime('%X', time.localtime(time.time()))
+print(f'{t} - Ответ получен: {buff_resp}')
 # Читаем полученый JSON в Pandas
 download_data = pd.read_json(json.dumps(buff_resp.json()['value']))
 
@@ -36,10 +38,10 @@ for rec in download_data.index:
 t = time.strftime('%X', time.localtime(time.time()))
 print(f'{t} - Готово! {rec} записей раскрыто.')
 
-# file_path = 'Z:\\Ежеквартальное планирование\\'
-file_path = 'C:\\ProgramData\\BaseGroup\\Loginom 6\\Server\\UserStorage\\konstantin.kutovoy\\Ежеквартальное планирование'
+file_path = 'Z:\\Для логистов и закупок\\'
+# file_path = 'C:\\ProgramData\\BaseGroup\\Loginom 6\\Server\\UserStorage\\konstantin.kutovoy\\Ежеквартальное планирование'
 # Записываем результат в файл
-col.to_csv(file_path+'CenaNomenklatury.csv', encoding='cp1251')
+col.to_csv(file_path+'CenaNomenklaturyPostavschikov.csv', encoding='cp1251')
 print('Файл записан')
 
 # Шлем радостную весть
