cd59f41361dd6f93f86a2a26fc640bda59202c56
BioVstu <biovstu@gmail.com>
2023-08-11 12:37:35 +0300

    upd sending in telegram

--- Logging/Send_in_teleg_if.py
+++ Logging/Send_in_teleg_if.py
@@ -1,5 +1,6 @@
 import requests
 import numpy as np, pandas as pd
+import urllib.parse
 
 input_frame = pd.DataFrame({})
     
@@ -7,6 +8,7 @@ input_frame = pd.DataFrame({})
 API_TOKEN = ''
 CHAT_ID = ''
 MESSAGE_TEXT = ''
+MESSAGE_TEXT = urllib.parse.quote(MESSAGE_TEXT)
 SEND_IF = ''
 if input_frame.shape[0] > SEND_IF:
     data = requests.get(f'https://api.telegram.org/bot{API_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={MESSAGE_TEXT}')
--- Logging/Sending_in_telegram.py
+++ Logging/Sending_in_telegram.py
@@ -1,7 +1,9 @@
 import requests
+import urllib.parse
 
 API_TOKEN = ''
 CHAT_ID = ''
 ERROR_TEXT = 'Верификация пройдена!\nПривет!'
+ERROR_TEXT = urllib.parse.quote(ERROR_TEXT)
 data = requests.get(f'https://api.telegram.org/bot{API_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={ERROR_TEXT}')
 print(data)
