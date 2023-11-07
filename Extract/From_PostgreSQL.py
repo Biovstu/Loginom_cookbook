6f566920220923d7cecec169a217efa9ae933ae8
BioVstu <biovstu@gmail.com>
2023-11-07 15:23:18 +0300

    cng PSQL to SQLAlchemy

--- Extract/From_PostgreSQL.py
+++ Extract/From_PostgreSQL.py
@@ -1,35 +1,26 @@
-import psycopg2
-from psycopg2 import OperationalError
 import pandas as pd
 
-db_name = ''
-db_user = ''
-db_password = ''
-db_host = ''
-db_port = ''
-table = ''
+import sqlalchemy as sqla
+from sqlalchemy.engine import URL
 
-try:
-    connection = psycopg2.connect(
-        database=db_name,
-        user=db_user,
-        password=db_password,
-        host=db_host,
-        port=db_port)
-    print("Connection to PostgreSQL DB successful")
-    try:
-        with connection.cursor() as cursor:
-            query = f'SELECT * FROM {table}'
-            cursor.execute(query)
-            result = cursor.fetchall()
-            desc = cursor.description
-    finally:
-        connection.close()
-except OperationalError as e:
-    print(f"The error '{e}' occurred")
+SERVER = ''
+LOGIN = ''
+PASS = ''
+DBNAME = ''
+TABLENAME = ''
+PORT = ''
 
-col = pd.DataFrame(desc)
-col[0] = col[0].apply(lambda x: x[0]) # выделяем только назвнаия столбцов
+connection_url = URL.create(
+    "postgresql",
+    username=LOGIN,
+    password=PASS,
+    host=SERVER,
+    port=PORT,
+    database=DBNAME,
+    query={},
+)
 
-# Создаем основную таблицу
-df = pd.DataFrame(result, columns=col[0]) 
+# запрос таблицы
+engine = sqla.create_engine(connection_url)
+with engine.connect() as conn:
+    df = pd.read_sql_table(TABLENAME, conn)
--- SandBox/test.py
+++ SandBox/test.py
@@ -1,10 +1,6 @@
-sp = [1,2,3,4,5,6,7,8,9]
+sp = [9,2,1,4,4,8,6,7,7,8,9,1]
 
-tmp = []
-for i in range(1, len(sp) + 1):
-    if i % 3 != 0:
-        tmp.append(sp[i-1])
-print(tmp)
-
-sp_new = [v for i,v in enumerate(sp) if (i + 1) % 3 != 0]
+sp_new = [v for i,v in enumerate(sp) if v not in sp[:i]]
 print(sp_new)
+print(list(set(sp)))
+print(sp[::2])
